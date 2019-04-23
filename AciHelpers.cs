ad7a9d4a882e1c5177b6b0c3f6e39f88e10c1c66
Mark Heath <mark.heath@gmail.com>
2019-04-23 14:10:53 +0100

    Adding fluent helpers

--- AciHelpers.cs
+++ AciHelpers.cs
@@ -59,59 +59,68 @@ namespace DurableFunctionsAci
             log.LogInformation($"Creating container group '{cg.ContainerGroupName}' with start command '{cg.CommandLine}'");
 
             // Get the resource group's region
-            
+
             IResourceGroup resGroup = await azure.ResourceGroups.GetByNameAsync(cg.ResourceGroupName);
             Region azureRegion = resGroup.Region;
             log.LogInformation($"Region is {azureRegion}");
             //Region.EuropeWest
             // Create the container group
-            var baseDefinition = azure.ContainerGroups.Define(cg.ContainerGroupName)
+            var vol = cg.AzureFileShareVolumes?.FirstOrDefault();
+            var containerGroupDefinition = azure.ContainerGroups.Define(cg.ContainerGroupName)
                 .WithRegion(azureRegion)
                 .WithExistingResourceGroup(cg.ResourceGroupName)
                 .WithLinux()
-                .WithPublicImageRegistryOnly();
+                .WithPublicImageRegistryOnly()
+                .WithOptionalVolume(vol)
+                .DefineContainerInstance(cg.ContainerGroupName + "-1")
+                    .WithImage(cg.ContainerImage)
+                    .WithExternalTcpPort(80)
+                    .WithCpuCoreCount(1.0)
+                    .WithMemorySizeInGB(1)
+                    .WithOptionalVolumeMount(vol)
+                    .WithOptionalCommandLine(log, cg.CommandLine)
+                    .WithEnvironmentVariables(cg.EnvironmentVariables)
+                    .Attach()
+                .WithDnsPrefix(cg.ContainerGroupName)
+                .WithRestartPolicy(ContainerGroupRestartPolicy.Never);
 
-            IWithFirstContainerInstance definition;
-            var vol = cg.AzureFileShareVolumes?.FirstOrDefault();
+            var containerGroup = await containerGroupDefinition.CreateAsync();
+            log.LogInformation($"Created container group {containerGroup.Name} with state {containerGroup.State}");
+            // Print the container's logs
+            //Console.WriteLine($"Logs for container '{containerGroupName}-1':");
+            //Console.WriteLine(await containerGroup.GetLogContentAsync(containerGroupName + "-1"));
+        }
+
+        private static IWithContainerInstanceAttach<IWithNextContainerInstance> WithOptionalCommandLine(this IWithContainerInstanceAttach<IWithNextContainerInstance> baseDefinition, ILogger log, string commandLine)
+        {
+            if (!String.IsNullOrEmpty(commandLine))
+            {
+                var args = SplitArguments(commandLine);
+                log.LogInformation("Command line: " + String.Join('|', args));
+                baseDefinition = baseDefinition.WithStartingCommandLine(args[0], args.Skip(1).ToArray());
+            }
+
+            return baseDefinition;
+        }
+
+        private static IWithContainerInstanceAttach<IWithNextContainerInstance> WithOptionalVolumeMount(this IWithContainerInstanceAttach<IWithNextContainerInstance> baseDefinition, AzureFileShareVolumeDefinition vol)
+        {
+            if (vol != null)
+                baseDefinition = baseDefinition.WithVolumeMountSetting(vol.VolumeName, vol.MountPath);
+            return baseDefinition;
+        }
+
+        private static IWithFirstContainerInstance WithOptionalVolume(this IWithPrivateImageRegistryOrVolume baseDefinition, AzureFileShareVolumeDefinition vol)
+        {
             if (vol != null)
             {
-                definition = baseDefinition.DefineVolume(vol.VolumeName)
+                return baseDefinition.DefineVolume(vol.VolumeName)
                     .WithExistingReadWriteAzureFileShare(vol.ShareName)
                     .WithStorageAccountName(vol.StorageAccountName)
                     .WithStorageAccountKey(vol.StorageAccountKey)
                     .Attach();
             }
-            else
-            {
-                definition = baseDefinition.WithoutVolume();
-            }
-            
-            var withInstance = definition.DefineContainerInstance(cg.ContainerGroupName + "-1")
-                    .WithImage(cg.ContainerImage)
-                    .WithExternalTcpPort(80)
-                    .WithCpuCoreCount(1.0)
-                    .WithMemorySizeInGB(1);
-
-            if (vol != null)
-                withInstance = withInstance.WithVolumeMountSetting(vol.VolumeName, vol.MountPath);
-
-            if (!String.IsNullOrEmpty(cg.CommandLine))
-            {
-                var args = SplitArguments(cg.CommandLine);
-                log.LogInformation("Command line: " + String.Join('|',args));
-                withInstance = withInstance.WithStartingCommandLine(args[0], args.Skip(1).ToArray());
-            }
-
-            var withCreate = withInstance
-                    .WithEnvironmentVariables(cg.EnvironmentVariables)
-                    .Attach()
-                .WithDnsPrefix(cg.ContainerGroupName)
-                .WithRestartPolicy(ContainerGroupRestartPolicy.Never);
-            
-            var containerGroup = await withCreate.CreateAsync();
-            // Print the container's logs
-            //Console.WriteLine($"Logs for container '{containerGroupName}-1':");
-            //Console.WriteLine(await containerGroup.GetLogContentAsync(containerGroupName + "-1"));
+            return baseDefinition.WithoutVolume();
         }
 
         public static async Task<ContainerGroupStatus> GetContainerGroupStatus(
